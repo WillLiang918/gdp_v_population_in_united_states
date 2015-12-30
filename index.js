a6418cff26be9eca2aa98d94d525f0eb660912b5
William <WilliamLiang918@gmail.com>
2015-12-30 01:12:56 -0500

    Represent data using logarithmic scale.

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-var svg = d3.select("body").append("svg")
-  .attr("width", 500)
-  .attr("height", 500);
+var outerWidth = 400;
+var outerHeight = 400;
+var rMin = 1;
+var rMax = 10;
+var xColumn = "POPESTIMATE2009";
+var yColumn = "GDP2009";
+var rColumn = "POPESTIMATE2009";
 
-var xScale = d3.scale.linear().range([0, 500]);
-var yScale = d3.scale.linear().range([500, 0]);
+var svg = d3.select("body").append("svg")
+  .attr("width", outerWidth)
+  .attr("height", outerHeight);
+
+// var xScale = d3.scale.linear().range([0, outerWidth]);
+// var yScale = d3.scale.linear().range([outerHeight, 0]);
+var xScale = d3.scale.log().range([0, outerWidth]);
+var yScale = d3.scale.log().range([outerHeight, 0]);
+var rScale = d3.scale.sqrt().range([rMin, rMax]);
 
 function render(data) {
 
-  xScale.domain(d3.extent(data, function (d){ return d.POPESTIMATE2009; }));
-  yScale.domain(d3.extent(data, function (d){ return d.GDP2009; }));
+  xScale.domain(d3.extent(data, function (d){ return d[xColumn]; }));
+  yScale.domain(d3.extent(data, function (d){ return d[yColumn]; }));
+  rScale.domain([0, d3.max(data, function (d){ return d[rColumn]; })]);
 
   var circles = svg.selectAll("circle").data(data);
-  circles.enter().append("circle").attr("r", 5);
+  circles.enter().append("circle");
 
   circles
-    .attr("cx", function (d){ return xScale(d.POPESTIMATE2009); })
-    .attr("cy", function (d){ return yScale(d.GDP2009); });
+    .attr("cx", function (d){ return xScale(d[xColumn]); })
+    .attr("cy", function (d){ return yScale(d[yColumn]); })
+    .attr("r", function (d){ return rScale(d[rColumn]); });
 
-  console.log(circles);
-  // circles.exit().remove();
+  circles.exit().remove();
 }
 
 function type(d){
@@ -27,4 +39,9 @@ function type(d){
   return d;
 }
 
-d3.csv("population.csv", type, render);
+d3.csv("population.csv", type, function (data) {
+  render(data);
+  var people = rScale.domain()[1];
+  var pixels = Math.PI * rMax * rMax;
+  console.log((people / pixels) + " people per pixel.");
+});
