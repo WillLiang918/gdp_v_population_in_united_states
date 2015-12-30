9ab7848f3a6e71c794d0430464d5144e0a6dda99
William <WilliamLiang918@gmail.com>
2015-12-30 00:52:14 -0500

    Map Population to GDP.

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ var svg = d3.select("body").append("svg")
   .attr("width", 500)
   .attr("height", 500);
 
-function render(data) {
-  var circles = svg.selectAll("circle").data(data);
+var xScale = d3.scale.linear().range([0, 500]);
+var yScale = d3.scale.linear().range([500, 0]);
 
-  circles.enter().append("circle")
-  .attr("r", 10);
+function render(data) {
+
+  xScale.domain(d3.extent(data, function (d){ return d.POPESTIMATE2009; }));
+  yScale.domain(d3.extent(data, function (d){ return d.GDP2009; }));
+
+  var circles = svg.selectAll("circle").data(data);
+  circles.enter().append("circle").attr("r", 5);
 
   circles
-    .attr("cx", function (d){ return d.POPESTIMATE2009; })
-    .attr("cy", function (d){ return d.GDP2009; });
+    .attr("cx", function (d){ return xScale(d.POPESTIMATE2009); })
+    .attr("cy", function (d){ return yScale(d.GDP2009); });
 
   console.log(circles);
   // circles.exit().remove();
 }
 
-d3.csv("population.csv", function (states) {
-  render(states);
-});
+function type(d){
+  d.POPESTIMATE2009 = +d.POPESTIMATE2009;
+  d.GDP2009 = +d.GDP2009;
+  return d;
+}
+
+d3.csv("population.csv", type, render);
